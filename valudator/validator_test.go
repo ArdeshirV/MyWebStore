a497998d8fad5f733ee0d6cd8e77253bc43c220f
ArdeshirV <e-job@protonmail.com>
2025-09-03 15:17:17 +0200

    Update

---
+++ valudator/validator_test.go
@@ -0,0 +1,19 @@
+package validator
+
+import (
+    "log"
+    "testing"
+)
+
+func TestPhoneNumber(t *testing.T) {
+    t.Run("valid", func(t *testing.T) {
+        for i, phone := range []string {
+                "09121234567",
+                "09129876543",
+                "09365432109",
+                "09217998990",
+            } {
+
+         }
+    })
+}
