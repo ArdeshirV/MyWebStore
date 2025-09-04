65616f156710ef10f47cc1ee21f16db773f38717
ArdeshirV <e-job@protonmail.com>
2025-09-04 10:17:58 +0200

    Update

--- main.go
+++ main.go
@@ -6,19 +6,4 @@ import (
 
 func main() {
 	fmt.Println(Prompt("My Web Store in Golang"))
-	fmt.Println(Out("Output: ", GreenBoldText("Green"), BoldText("White"), RedBoldText("Red")))
-
-	fmt.Println()
-	fmt.Print(Prompt("Enter your age: ") + InputColor())
-	var age int
-	fmt.Scanf("%d", &age)
-	fmt.Print(Out("Age("), In(age), Out(") = "), Result(GetAlphabeticNumber(age)), "\n")
-}
-
-func GetAlphabeticNumber(value int) string {
-	if value == 40 {
-		return "Fourty"
-	} else {
-		return "Unknown"
-	}
 }
