b2ed533206a214296fbd9f5fec5f01068c877597
ArdeshirV <e-job@protonmail.com>
2025-09-06 13:56:43 +0200

    Update

--- validator/validator.go
+++ validator/validator.go
@@ -14,50 +14,48 @@ const (
 )
 
 type (
-    Rule func() error
+	Rule func() error
 
-    Validator interface {
-        AddRule(Rule) Validator
-        Validate() error
-    }
+	Validator interface {
+		AddRule(Rule) Validator
+		Validate() error
+	}
 
-    ValidatorError struct {
-        errors []error
-        strings []string
-    }
+	ValidatorError struct {
+		errors  []error
+		strings []string
+	}
 
-    validator struct {
-        rules []Rule
-    }
+	validator struct {
+		rules []Rule
+	}
 )
 
 func New() Validator {
-    return &validator {
-        rules: make([]Rule, 0),
-    }
+	return &validator{
+		rules: make([]Rule, 0),
+	}
 }
 
 func (v *validator) AddRule(rule Rule) {
-    v.rules = append(v.rules, rule)
-    return v
+	v.rules = append(v.rules, rule)
+	return v
 }
 
 func (v *validator) Validate() error {
-    ret := new(ValidatorError)
-    for _, rule := range v.rules {
-        if err := rule(); err != nil {
-            ret.errors = append(ret.errors, err)
-            ret.strings = append(ret.strings, err.Error())
-        }
-    }
-    if len(ret.errors) == 0 {
-        return nil
-    }
-    return ret
+	ret := new(ValidatorError)
+	for _, rule := range v.rules {
+		if err := rule(); err != nil {
+			ret.errors = append(ret.errors, err)
+			ret.strings = append(ret.strings, err.Error())
+		}
+	}
+	if len(ret.errors) == 0 {
+		return nil
+	}
+	return ret
 }
 
 func (v *ValidatorError) Error() stirng {
-    return strings.Join(v.strings, ", ")
+	return strings.Join(v.strings, ", ")
 }
-
-
