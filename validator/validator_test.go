0f20a159391cbd89078e75f02ec521bf347cc907
ArdeshirV <e-job@protonmail.com>
2025-09-06 12:42:37 +0200

    Add validator_test.TestPhoneNumber(t)

--- validator/validator_test.go
+++ validator/validator_test.go
@@ -12,8 +12,31 @@ func TestPhoneNumber(t *testing.T) {
                 "09129876543",
                 "09365432109",
                 "09217998990",
-            } {
-
+         } {
+            validator := New()
+            err := validator.AddRule(Phone(phone)).Validate()
+            if err != nil {
+                t.Fatalf("[test case %d] expected nil got %v", i, err)
+            }
          }
     })
+
+    t.Run("invalid", func(t *testing.T) {
+        for i, phone := range []string {
+            "",
+            "0912",
+            "0936",
+            "1234567",
+            "0214567895",
+            "sdf349f9sdf",
+        }{
+            validator := New()
+            err := validator.AddRule(Phone(phone)).Validate()
+            if err != nil {
+                t.Fatalf("[test case %d] expected nil got %v", i, err)
+            }
+        }
+    })
 }
+
+
