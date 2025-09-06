31d98ba6edeb9d3f3c2377bfbd02ac3941b7aa33
ArdeshirV <e-job@protonmail.com>
2025-09-06 15:26:47 +0200

    Add product_test.go - var(...)

--- services/product_test.go
+++ services/product_test.go
@@ -14,11 +14,42 @@ func AssertNotError(t *testing.T, err error, i int) {
     t.Fatalf("[test case %d] expected nil got %v", i, err)
 }
 
-func AssertError(t testing.T, err error, i int) {
+func AssertError(t *testing.T, err error, i int) {
     if err != nil {
         return
     }
     t.Fatalf("[test case %d] expected error got nil", i)
 }
+
+var (
+    productStorage = storage.NewProductInMemory()
+    productService = services.NewProduct(productStorage)
+    testProducts = []*models.Product{
+        {
+            Price: 1000,
+            Weight: 1.5,
+            Title: "Samsung Galaxy S24",
+            CategoryID: 1,
+            PDF: "file1.pdf",
+            Description: "Perfect product!",
+        },
+        {
+            Price: 2000,
+            Weight: 5.7,
+            Title: "Playstation 5 Pro",
+            CategoryID: 2,
+            PDF: "file2.pdf",
+            Description: "Buy it now!",
+        },
+        {
+            Price: 3000,
+            Weight: 2.1,
+            Title: "Galaxt Buds Pro",
+            CategoryID: 3,
+            PDF: "file3.pdf",
+            Description: "The best price in the market!",
+        },
+    }
+)
 
 
