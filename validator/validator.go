2df31be0216b1208372c6228c4d78a6d5fbe1cbf
ArdeshirV <e-job@protonmail.com>
2025-09-06 13:28:56 +0200

    Add validator.type(...)

---
+++ validator/validator.go
@@ -0,0 +1,16 @@
+package validator
+
+import (
+	"string"
+)
+
+const (
+	ErrFieldIsEmpty     = "field is empty"
+	ErrAlreadyHasID     = "field ID is exists, use update"
+	ErrProductExists    = "this product is registered"
+	ErrProductNotFound  = "product not found"
+	ErrCategoryNotFound = "category not found"
+	ErrCategoryExists   = "category is exists"
+)
+
+type ()
