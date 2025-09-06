4ea994edda92f1053f7112469174935652d55bd6
ArdeshirV <e-job@protonmail.com>
2025-09-06 15:18:00 +0200

    Add product_test.go

---
+++ services/product_test.go
@@ -0,0 +1,10 @@
+package services_test
+
+import (
+    "testing"
+    "github.com/ArdeshirV/MyWebStore/models"
+    "github.com/ArdeshirV/MyWebStore/services"
+    "github.com/ArdeshirV/MyWebStore/storage"
+)
+
+
