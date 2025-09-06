e94bba070312b5b3bc17317f57663f0de65fb232
ArdeshirV <e-job@protonmail.com>
2025-09-06 14:03:55 +0200

    Add rules.go - Phone(string) Rule

--- validator/rules.go
+++ validator/rules.go
@@ -8,4 +8,24 @@ import (
     "golang.org/x/exp/constraints"
 )
 
+func Phone(value string) Rule {
+    return func() error {
+        if err := String("phone", value)(); err != nil {
+            return err
+        }
+        value = strings.Map(func(r rune) rune {
+            if unicode.IsDigit(r) {
+                return r
+            }
+            return -1
+        }, value)
+
+        if strings.HasPrefix(value, "09") && len(value) == 11 {
+            return nil
+        }
+
+        return errors.New("phone number is not valid")
+    }
+} 
+
 
